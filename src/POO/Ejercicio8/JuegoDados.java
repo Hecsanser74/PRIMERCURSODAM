9e9bc12b93f323192a9bd0a8be0203fdab29423c
Hecsanser74 <hectorsanse16@gmail.com>
2025-02-06 16:23:05 +0100

    Ejercicio 8 POO Acabado

--- src/POO/Ejercicio8/JuegoDados.java
+++ src/POO/Ejercicio8/JuegoDados.java
@@ -7,34 +7,38 @@ public class JuegoDados {
     Dado dado3;
 
     //Primer metodo
-    private void defineDados(Dado dado1, Dado dado2, Dado dado3){
-        dado1 = new Dado((int) (Math.random()*6)+1);
-        dado2 = new Dado((int) (Math.random()*6)+1);
-        dado3 = new Dado((int) (Math.random()*6)+1);
+   public void defineDados(){
+        dado1 = new Dado((int) (Math.random() * 6 + 1));
+        dado2 = new Dado((int) (Math.random()* 6 + 1));
+        dado3 = new Dado((int) (Math.random() * 6 + 1));
     }
 
     //Segundo Metodo
     public void tiroDado() {
-        dado1.setValorDado((int)(Math.random()+6)+1);
-        dado2.setValorDado((int)(Math.random()*6)+1);
-        dado3.setValorDado((int)(Math.random()*6)+1);
+        dado1.setValorDado((int)(Math.random() * 6 + 1));
+        dado2.setValorDado((int)(Math.random() * 6 + 1));
+        dado3.setValorDado((int)(Math.random() * 6 + 1));
     }
 
     //Tercer Metodo
-    public void main(String[] args){
+    public static void main(String[] args){
 
-        defineDados(dado1, dado2, dado3);
+        JuegoDados juegoDeDados = new JuegoDados();
+
+        juegoDeDados.defineDados();
 
         int contador = 0;
 
         do {
-            System.out.println(dado1.getValorDado()+" - "+dado2.getValorDado()+" - "+dado3.getValorDado());
+            System.out.println(juegoDeDados.dado1.getValorDado()+" - "+juegoDeDados.dado2.getValorDado()+" - "+juegoDeDados.dado3.getValorDado());
 
-            if (dado1.getValorDado() == dado2.getValorDado() && dado1.getValorDado() == dado3.getValorDado()){
-                System.out.println("Lo has hecho en el intento: "+contador);
+            if (juegoDeDados.dado1.getValorDado() == juegoDeDados.dado2.getValorDado() && juegoDeDados.dado1.getValorDado() == juegoDeDados.dado3.getValorDado()){
+                System.out.println("Lo has hecho en el intento: "+(contador + 1));
+                break;
+            } else {
+                contador++;
+                juegoDeDados.tiroDado();
             }
-
-            contador++;
         } while (true);
     }
 }
